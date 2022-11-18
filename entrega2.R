629a688a735db1e78b332b317739b5518550b66c
DauiGV <118559705+DauiGV@users.noreply.github.com>
2022-11-18 19:28:07 +0100

    Update entrega2.R

--- entrega2.R
+++ entrega2.R
@@ -1,10 +1,23 @@
 df<- read.csv("enquesta.csv")
-head(df)
-mean(df$TABAC)
-table(df$TABAC)
-df$OCI
-table(df$OCI)
-sum<-0
-if (df$OCI==4 & df$TABAC==1):
-  sum<-sum+1
-  
+#1a
+mean(df$TABAC)#probabilidad de que una persona fume
+#
+mean(df$OCI==4 & df$TABAC==1)
+mean(df$OCI==3 & df$TABAC==1)
+mean(df$OCI==2 & df$TABAC==1)
+mean(df$OCI==1 & df$TABAC==1)
+#viendo los resultados fuman menos
+#1b
+d160<-mean(df$ALTURA>=160 & df$SEXE=="d")
+x<-mean((df$OCI==1 | df$OCI==2) & df$ALTURA>=160 & df$SEXE=="d")
+(x/d160)*100
+#1c
+hof<-mean(df$OCI==4 & df$TABAC==1 & df$SEXE=="h")
+
+fumon<-mean(df$TABAC==1)
+hof/fumon*100
+#1d
+mean(df$OCI==4 & df$TABAC==0)
+mean((df$OCI==2 | df$OCI==3) & df$TABAC==1)
+#1e
+
